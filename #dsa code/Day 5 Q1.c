eee6225af671a7839b162cf6a4a15c7a87c85819
madhup123keshav-sys <madhup123keshav@gmail.com>
2026-02-12 23:28:53 +0530

    Create Day 5 Q1.c

---
+++ #dsa code/Day 5 Q1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int p, q;
+    scanf("%d", &p);
+
+    int a[1000];
+    for (int i = 0; i < p; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    scanf("%d", &q);
+
+    int b[1000];
+    for (int i = 0; i < q; i++) {
+        scanf("%d", &b[i]);
+    }
+
+    int i = 0, j = 0;
+
+    while (i < p && j < q) {
+        if (a[i] <= b[j]) {
+            printf("%d ", a[i]);
+            i++;
+        } else {
+            printf("%d ", b[j]);
+            j++;
+        }
+    }
+
+    while (i < p) {
+        printf("%d ", a[i]);
+        i++;
+    }
+
+    while (j < q) {
+        printf("%d ", b[j]);
+        j++;
+    }
+
+    return 0;
+}
