d337309e1c1dbc9db274e4d3ef6cbc241b444eed
madhup123keshav-sys <madhup123keshav@gmail.com>
2026-04-03 12:00:18 +0530

    Create Day 62 Q1.c

---
+++ #dsa code/Day 62 Q1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    struct Node** adj = (struct Node**)malloc(n * sizeof(struct Node*));
+
+    for (int i = 0; i < n; i++)
+        adj[i] = NULL;
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+
+        struct Node* newNode = createNode(v);
+        newNode->next = adj[u];
+        adj[u] = newNode;
+
+        newNode = createNode(u);
+        newNode->next = adj[v];
+        adj[v] = newNode;
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d:", i);
+        struct Node* temp = adj[i];
+        while (temp) {
+            printf(" %d", temp->vertex);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
