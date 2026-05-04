7c99548ebf08a31026e6d7df4316ea6700b3c532
madhup123keshav-sys <madhup123keshav@gmail.com>
2026-05-04 10:07:44 +0530

    Create Day 29 Q1.c

---
+++ #dsa code/Day 29 Q1.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* insertEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+struct Node* rotateRight(struct Node* head, int k) {
+    if (head == NULL || head->next == NULL || k == 0)
+        return head;
+
+    int n = 1;
+    struct Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+        n++;
+    }
+
+    k = k % n;
+    if (k == 0)
+        return head;
+
+    temp->next = head;
+
+    int steps = n - k;
+    struct Node* newTail = head;
+    for (int i = 1; i < steps; i++)
+        newTail = newTail->next;
+
+    struct Node* newHead = newTail->next;
+    newTail->next = NULL;
+
+    return newHead;
+}
+
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, k, value;
+    struct Node* head = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        head = insertEnd(head, value);
+    }
+
+    scanf("%d", &k);
+
+    head = rotateRight(head, k);
+    printList(head);
+
+    return 0;
+}
