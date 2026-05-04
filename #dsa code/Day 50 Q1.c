43a2f5909781f5fb41cbc8b0a892b454833f6a6a
madhup123keshav-sys <madhup123keshav@gmail.com>
2026-05-04 10:07:45 +0530

    Create Day 50 Q1.c

---
+++ #dsa code/Day 50 Q1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    while (root != NULL) {
+        if (root->val == val)
+            return root;
+        else if (val < root->val)
+            root = root->left;
+        else
+            root = root->right;
+    }
+    return NULL;
+}
+
+int main() {
+    struct TreeNode* root = newNode(4);
+    root->left = newNode(2);
+    root->right = newNode(7);
+    root->left->left = newNode(1);
+    root->left->right = newNode(3);
+
+    int target = 2;
+    struct TreeNode* result = searchBST(root, target);
+
+    if (result != NULL)
+        printf("%d\n", result->val);
+    else
+        printf("Not found\n");
+
+    return 0;
+}
