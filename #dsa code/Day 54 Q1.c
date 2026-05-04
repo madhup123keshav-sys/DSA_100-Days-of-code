54584e982644dd396be42d0929662298cbdd025c
madhup123keshav-sys <madhup123keshav@gmail.com>
2026-05-04 10:07:45 +0530

    Create Day 54 Q1.c

---
+++ #dsa code/Day 54 Q1.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left, *right;
+} Node;
+
+Node* newNode(int data) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    Node** queue = (Node**)malloc(n * sizeof(Node*));
+    int front = 0, rear = 0;
+
+    Node* root = newNode(arr[0]);
+    queue[rear++] = root;
+
+    int i = 1;
+
+    while (front < rear && i < n) {
+        Node* curr = queue[front++];
+
+        if (arr[i] != -1) {
+            curr->left = newNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = newNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    free(queue);
+    return root;
+}
+
+void zigzagTraversal(Node* root) {
+    if (!root) return;
+
+    Node** queue = (Node**)malloc(1000 * sizeof(Node*));
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+    int leftToRight = 1;
+
+    while (front < rear) {
+        int size = rear - front;
+        int* level = (int*)malloc(size * sizeof(int));
+
+        for (int i = 0; i < size; i++) {
+            Node* curr = queue[front++];
+
+            if (leftToRight)
+                level[i] = curr->data;
+            else
+                level[size - i - 1] = curr->data;
+
+            if (curr->left) queue[rear++] = curr->left;
+            if (curr->right) queue[rear++] = curr->right;
+        }
+
+        for (int i = 0; i < size; i++)
+            printf("%d ", level[i]);
+
+        free(level);
+        leftToRight = !leftToRight;
+    }
+
+    free(queue);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int* arr = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    Node* root = buildTree(arr, n);
+    zigzagTraversal(root);
+
+    free(arr);
+    return 0;
+}
